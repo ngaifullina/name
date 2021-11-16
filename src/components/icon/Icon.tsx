6e960b6e3453dbed77740527071439a3402df51a
Nelli Gaifullina <nellie.mailbox@gmail.com>
2021-11-16 17:00:37 +0300

    Icons

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,19 +1,23 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-
-import { useAppSelector, useAppDispatch } from "../../app/hooks";
-import { addTodo, selectInputValue, editInputValue } from "../todos/slice";
-// import styles from "./Todos.module.css";
-import { TodosList } from "../todo-list/TodosList";
+import styles from "./Icon.module.css";
 
 export type Props = {
   type: "delete" | "edit" | "tick" | "cross";
+  handleClick: () => void;
 };
 
 // all icons should
 // - have same size
 // - be inline-block
 // - same vertical alignment
-export function Icon() {
-  return <div></div>;
+
+export function Icon({ type, handleClick }: Props) {
+  return (
+    <img
+      src={`${type}.svg`}
+      alt=""
+      className={styles.icon}
+      onClick={handleClick}
+    />
+  );
 }
